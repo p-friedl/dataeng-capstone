0f7111a81b2e4e06dd6304146e2cce243996a05a
Patrik Friedl <patrik.friedl@gmail.com>
2020-08-09 23:22:02 +0200

    update to test iso3166-2 normalization

--- ISO_Normalization/geocode_test.py
+++ ISO_Normalization/geocode_test.py
@@ -1,6 +1,6 @@
 import googlemaps
 
-location = "Turks and Caicos Islands (the)"
+location = "Biliran, Philippines"
 # create googlemaps client - enter your API key
 # info: https://developers.google.com/maps/documentation/geocoding/get-api-key
 gmaps = googlemaps.Client(key='ENTER YOUR API KEY HERE')
@@ -13,21 +13,21 @@ if len(geocode_result) > 0:
     # iterate results
     for element in geocode_result:
         # only consider country results
-        if 'country' in element['types']:
+        if 'administrative_area_level_1' in element['types']:
             # extract normalized location name
             for component in element['address_components']:
-                if 'country' in component['types']:
+                if 'administrative_area_level_1' in component['types']:
                     norm_location = component['long_name']
 
             # extract coords
             lat = element['geometry']['location']['lat']
             long = element['geometry']['location']['lng']
         else:
-            norm_location = "No country element found"
+            norm_location = "No admin1 region element found"
             lat = 0.0
             long = 0.0
 else:
-    norm_location = "Not results found"
+    norm_location = "No results found"
     lat = 0.0
     long = 0.0
 
