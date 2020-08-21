2ed04b47c01f04e862130ecd0cd1a53bbd0f7068
Patrik Friedl <patrik.friedl@gmail.com>
2020-08-21 03:48:18 +0200

    fix utf-8 encoding bug, add data schema check

--- data_sources/csse_covid_19_daily_reports/harmonize_csse_data.py
+++ data_sources/csse_covid_19_daily_reports/harmonize_csse_data.py
@@ -37,6 +37,17 @@ def rename_or_create(df, oldcol, newcol):
             df[newcol] = np.nan
     return df
 
+def check_and_convert_dtype(df, col, dtype):
+    """
+    Compare current dtype with reference and convert if needed.
+    """
+    # handle missing values for int
+    if 'int' in dtype:
+        df[col] = df[col].fillna(0)
+    # if dtype not as expected -> convert dtype
+    if df[col].dtypes != dtype:
+        df[col] = df[col].astype(dtype)
+    return df
 
 def main():
     """
@@ -55,7 +66,7 @@ def main():
                                                date_parts[0],
                                                date_parts[1])
             # read current csv file
-            df = pd.read_csv(file)
+            df = pd.read_csv(file, encoding="utf-8-sig")
             # add new date column
             if 'Date' not in df.columns:
                 df['Date'] = date_formatted
@@ -80,6 +91,23 @@ def main():
             # ensure all CSV columns are ordered in the same way
             df = df.sort_index(axis=1)
 
+            # set expected output data schema
+            schema = {'Active': 'int64',
+                      'Admin2': 'object',
+                      'Confirmed': 'int64',
+                      'Country_Region': 'object',
+                      'Date': 'datetime64',
+                      'Deaths': 'int64',
+                      'FIPS': 'int64',
+                      'Lat': 'float64',
+                      'Long_': 'float64',
+                      'Province_State': 'object',
+                      'Recovered': 'int64'}
+
+            # enforce data schema
+            for col, dtype in schema.items():
+                check_and_convert_dtype(df, col, dtype)
+            
             # rewrite current csv
             df.to_csv(file, index=False)
 
